4b959837268cac7d022535f47fb6363e6c1c1d64
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2019-06-27 11:16:02 +0300

    Initial commit (provide basic app implementation)

---
+++ README.md
@@ -0,0 +1,15 @@
+# hyperapp example app
+
+The `hyperapp`-based example app for the Toptal non-mainstream framework comparison article.
+
+## Quickstart
+
+1) First, install everything with `yarn`:
+
+        yarn install
+
+2) Then start the app (with [parcel](https://parceljs.org/)):
+
+        yarn start
+
+1) There's no step 3. Open up a browser at [`localhost:1234`](http://localhost:1234).
---
+++ src/index.jsx
@@ -0,0 +1,111 @@
+import { h, app } from 'hyperapp';
+import { Http } from 'hyperapp-fx';
+import hyperappLogo from '../public/hyperapp-new-logo.png';
+
+// NOTE: Once the package is published, it would be imported with
+// import { preventDefault } from '@hyperapp/events';
+const preventDefault = [
+  function(_, props, event) {
+    if (props == null || props === true) event.preventDefault();
+  }
+];
+
+function fetchPostList(search) {
+  return Http({
+    url: `https://codingthat-quick-json-back-end-2.glitch.me/posts?q=${search}`,
+    action: (state, responseData) => ({
+      ...state,
+      posts: responseData,
+      isLoading: false
+    })
+  });
+}
+
+function getTargetValue(event) {
+  return event.target.value;
+}
+
+function inputChange(state, search) {
+  return { ...state, search };
+}
+
+function formSubmit(state) {
+  return [
+    { ...state, isLoading: true },
+    preventDefault,
+    fetchPostList(state.search)
+  ];
+}
+
+function init() {
+  return [
+    {
+      isLoading: true,
+      search: 'engineering',
+      posts: []
+    },
+    fetchPostList('engineering')
+  ];
+}
+
+const PostList = props => (
+  <div className="post-list">
+    {props.posts.map(post => (
+      <a className="post-list-item" href={post.url}>
+        {post.title}
+      </a>
+    ))}
+  </div>
+);
+
+app({
+  init,
+  view: state => (
+    <div className="layout">
+      <header className="app-header">
+        <div className="layout-container">
+          <div className="app-header-content">
+            <div className="header-image">
+              <img src={hyperappLogo} />
+            </div>
+
+            <form
+              className="search-form"
+              noValidate={true}
+              onSubmit={formSubmit}
+            >
+              <input
+                type="text"
+                className="search-input"
+                value={state.search}
+                oninput={[inputChange, getTargetValue]}
+              />
+
+              <button type="submit" className="search-btn">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </header>
+
+      <div className="layout-container">
+        <article className="app-main">
+          {state.isLoading ? (
+            <div className="list-loader">
+              <div className="spinner" />
+            </div>
+          ) : (
+            <PostList posts={state.posts} />
+          )}
+        </article>
+
+        <footer className="app-footer">
+          Made with ❤️and{' '}
+          <a href="https://github.com/jorgebucaran/hyperapp">hyperapp</a>
+        </footer>
+      </div>
+    </div>
+  ),
+  node: document.getElementById('app')
+});
